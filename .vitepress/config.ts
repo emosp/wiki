360c374fd24a70e26ae535e8947a5a598737065e
puzzle <happypuzzle@126.com>
2025-10-19 10:31:33 +0800

    change description

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: 'emos wiki',
+  description: 'emos wiki',
   srcDir: './docs',
   lang: 'zh-CN',
   base: '/',
